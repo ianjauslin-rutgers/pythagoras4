274e6d41788cf3f4f97e515d5ea52f230ccbc208
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-03-29 20:30:11 -0400

    add migration cheatsheet

---
+++ migration_cheatsheet.lean
@@ -0,0 +1,20 @@
+/-!
+
+Here are some tips on migrating
+
+* There are no ',' anymore!
+
+* There are no 'begin' or 'end' anymore
+
+* Indentation is important!
+
+* by_contradiction is now by_contra
+
+* You can no longer do
+    have := blabla _ _
+  instead, you need to do
+    have' := blabla _ _
+
+* '⬝' does something (I would like someone to explain what). 
+
+-/
