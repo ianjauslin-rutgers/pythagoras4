0ab0d2358dff5a04b4e01b28ac13866f5ba694e7
Alex Kontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2023-03-30 14:32:18 -0400

    more progress

--- Euclid/pythagoras_2.lean
+++ Euclid/pythagoras_2.lean
@@ -12,63 +12,45 @@ lemma ne_13_of_not_colinear {a b c : point} (tri_abc : ¬ colinear a b c) : a �
   have aL : online a L := by rwa [ac.symm] at cL
   exact tri_abc ⟨L, aL, bL, cL⟩ 
 
-lemma ne_12_of_not_colinear {a b c : point} (tri_abc : ¬ colinear a b c) : a ≠ b := 
- by sorry
- --
-/-begin
-  intros ab,
-  obtain ⟨L, bL, cL⟩ := line_of_pts b c,
-  refine tri_abc ⟨L, _, bL, cL⟩,
-  rwa ab,  end-/
+lemma ne_12_of_not_colinear {a b c : point} (tri_abc : ¬ colinear a b c) : a ≠ b := by
+  intro ab
+  obtain ⟨L, bL, cL⟩ := line_of_pts b c
+  have aL : online a L := by rwa [ab.symm] at bL
+  exact tri_abc ⟨L, aL, bL, cL⟩ 
 
-lemma ne_23_of_not_colinear {a b c : point} (tri_abc : ¬ colinear a b c) : b ≠ c := 
- by sorry
- --
-/-begin
-  intros bc,
-  obtain ⟨L, aL, bL⟩ := line_of_pts a b,
-  refine tri_abc ⟨L, aL, bL, _⟩,
-  rwa ← bc,  end-/
+lemma ne_23_of_not_colinear {a b c : point} (tri_abc : ¬ colinear a b c) : b ≠ c := by
+  intro bc
+  obtain ⟨L, aL, bL⟩ := line_of_pts a b
+  have cL : online c L := by rwa [bc] at bL
+  exact tri_abc ⟨L, aL, bL, cL⟩ 
 
-lemma not_colinear_T {a b c : point} (tri_abc : ¬ colinear a b c) : ¬ colinear b c a :=
- by sorry
- --
-/-begin
-  rintros ⟨L, bL, cL, aL⟩,
-  exact tri_abc ⟨L, aL, bL, cL⟩,  end-/
+lemma not_colinear_T {a b c : point} (tri_abc : ¬ colinear a b c) : ¬ colinear b c a := by
+  exact fun ⟨L, bL, cL, aL⟩ => tri_abc ⟨L, aL, bL, cL⟩
 
-lemma not_colinear_R {a b c : point} (tri_abc : ¬ colinear a b c) : ¬ colinear b a c := 
- by sorry
- --
-/-begin
-  rintros ⟨L, aL, bL, cL⟩,
-  exact tri_abc ⟨L, bL, aL, cL⟩,  end-/
+lemma not_colinear_R {a b c : point} (tri_abc : ¬ colinear a b c) : ¬ colinear b a c := by 
+  exact fun ⟨L, aL, bL, cL⟩ => tri_abc ⟨L, bL, aL, cL⟩
 
 /-- Aux lemmata -/
 
 lemma aux_1 {a b c : point} (tri_abc : ¬ colinear a b c) (ang_a : rightangle ≤ angle b a c) : 
-  ∃ d, B b d c ∧ angle b d a = rightangle := 
- by sorry
- --
-/-begin
-  obtain ⟨L, bL, cL⟩ := line_of_pts b c,
-  have naL : ¬ online a L := λ h, tri_abc ⟨L, h, bL, cL⟩,
-  obtain ⟨d, hd₁, hd₂, hd₃⟩ := pythlem (ne_23_of_not_colinear tri_abc).symm cL bL naL ang_a,
-  refine ⟨d, B_symm hd₃, _⟩,
-  rwa angle_symm,  end-/
+  ∃ d, B b d c ∧ angle b d a = rightangle := by
+  obtain ⟨L, bL, cL⟩ := line_of_pts b c
+  have naL : ¬ online a L := by exact fun h => tri_abc ⟨L, h, bL, cL⟩
+  obtain ⟨d, hd₁, hd₂, hd₃⟩ := pythlem (ne_23_of_not_colinear tri_abc).symm cL bL naL ang_a
+  use d
+  rw [angle_symm]
+  exact ⟨B_symm hd₃, hd₂⟩ 
 
-lemma aux_2 {a b c d : point} (a_ne_b : a ≠ b) (Bbdc : B b d c) : angle a b d = angle a b c := 
- by sorry
- --
-/-begin
-  convert angle_extension_of_B a_ne_b.symm Bbdc using 1;
-  exact angle_symm _ _ _,  end-/
+lemma aux_2 {a b c d : point} (a_ne_b : a ≠ b) (Bbdc : B b d c) : angle a b d = angle a b c := by 
+  have := angle_extension_of_B a_ne_b.symm Bbdc
+  rwa [angle_symm a b d, angle_symm a b c]
 
 /-- *** Euclid VI.31 *** -/
 theorem pythagorean_proof_two {a b c : point} (tri_abc : ¬ colinear a b c) 
   (ang_a : angle b a c = rightangle) : 
-  (length a b)^2 + (length a c)^2 = (length b c)^2 :=
- by sorry
+    (length a b)^2 + (length a c)^2 = (length b c)^2 := by 
+  have ba : length b a ≠ 0 := length_eq_zero_iff.not.mpr (ne_12_of_not_colinear tri_abc).symm
+  sorry
  --
 /-begin
   have ba : length b a ≠ 0 := length_eq_zero_iff.not.mpr (ne_12_of_not_colinear tri_abc).symm,
