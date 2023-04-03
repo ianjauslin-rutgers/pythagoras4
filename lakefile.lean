e6fdfe93cadbf986c72bf5c96c93d3755a41e692
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-04-03 14:48:03 -0400

    Create package basefile to enable builds using 'lake build'

--- lakefile.lean
+++ lakefile.lean
@@ -4,10 +4,6 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
 
-package pythagoras {
-  -- add package configuration options here
-}
+package pythagoras
 
-lean_lib Euclid {
-  -- add library configuration options here
-}
+lean_lib Euclid
