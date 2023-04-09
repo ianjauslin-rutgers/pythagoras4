01c2cfe5c5cc327bf170382008f56d22c66a04a7
Alex Kontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2023-04-09 12:34:03 -0400

    definition change

--- Pythagoras/convex_polygon.lean
+++ Pythagoras/convex_polygon.lean
@@ -1,5 +1,5 @@
 import SyntheticEuclid4
-import Mathlib.Data.ZMod.Basic
+import Mathlib.Data.Fin.Basic
 
 open incidence_geometry
 
@@ -7,10 +7,7 @@ variables [i: incidence_geometry]
 
 def WeakSameside (a b : point) (L : line) : Prop := sameside a b L ∨ online a L ∨ online b L 
 
-class ConvexPolygon (n : ℕ) := 
-  (vertex : ZMod n → point)
-  (distinct : ∀ i : ZMod n, vertex i ≠ vertex (i+1))
-  (convex : ∀ i j k : ZMod n, WeakSameside (vertex j) (vertex k) 
+def IsConvexPolygon (n : ℕ) (vertex : Fin n → point) : Prop :=
+(distinct : ∀ i : Fin n, vertex i ≠ vertex (i+1)) ∧ (convex : ∀ i j k : Fin n, WeakSameside (vertex j) (vertex k) 
   (line_of_pts (vertex i) (vertex (i+1))))
 
-  
