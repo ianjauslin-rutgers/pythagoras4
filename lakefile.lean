a819bedab2eeabe47cc6679889ef88bc15b97529
Ian Jauslin <ian.jauslin@rutgers.edu>
2023-04-25 14:30:49 -0400

    Update synthetic

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
 require synthetic_euclid_4 from git
-  "https://github.com/ah1112/synthetic_euclid_4.git"@"0a915f9"
+  "https://github.com/ah1112/synthetic_euclid_4.git"@"1912710"
 
 package pythagoras
 
