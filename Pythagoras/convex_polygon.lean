270174e7e26f900c2e7b826b848cb96d1b4ae44e
Vladimir Sedlacek <vlada.sedlacek@mail.muni.cz>
2023-06-21 18:25:11 -0400

    Make triangulation a structure field instead of a parameter.

--- Pythagoras/convex_polygon.lean
+++ Pythagoras/convex_polygon.lean
@@ -65,8 +65,8 @@ def exterior_triangle (a b x : point) (V : List point) : Prop :=
 def convex_triangulation (V : List point) (S : List Triangle) : Prop :=
   -- TODO: match V.reverse, S.reverse with
   match V, S with
-  | _, [] => False
   | [], _ => False
+  | _, [] => False
   | [a, b, c], [T] => triangle_eq_of_pts a b c T
   | x :: V', T :: S' => convex_triangulation V' S' ∧ exterior_triangle T.a T.b x V' ∧ x = T.c
   termination_by convex_triangulation V S => V.length
@@ -77,31 +77,35 @@ def triangulation_area (S : List Triangle) : ℝ :=
   | [] => 0
   | T :: S' => area T.a T.b T.c + triangulation_area S'
 
-structure ConvexPolygon (V : List point) (S : List Triangle) where
-  convex : convex_triangulation V S
+structure ConvexPolygon (V : List point) where
+  triangulation : List Triangle
+  convex : convex_triangulation V triangulation
 
 namespace ConvexPolygon
 
-def area (_ : ConvexPolygon V S) : ℝ := triangulation_area S
+def area (P : ConvexPolygon V) : ℝ := triangulation_area P.triangulation
 
-def n (_ : ConvexPolygon V S) : ℕ := V.length
+def n (_ : ConvexPolygon V) : ℕ := V.length
 
-lemma vertices_ne_nil (P : ConvexPolygon V S) : V ≠ [] := by
+lemma vertices_ne_nil (P : ConvexPolygon V) : V ≠ [] := by
+  have S := P.triangulation
   have C := P.convex
   match V, S with
-  | [], [] => simp [convex_triangulation] at C
-  | [], _ :: _ => simp [convex_triangulation] at C
-  | _ :: _, _ => simp
-
-lemma triangulation_ne_nil (P : ConvexPolygon V S) : S ≠ [] := by
-  have C := P.convex
-  match S, V with
   | [], _ => simp [convex_triangulation] at C
   | _ :: _, _ => simp
 
-lemma nodup (P : ConvexPolygon V S) : Nodup V := by
+lemma triangulation_ne_nil (P : ConvexPolygon V) : P.triangulation ≠ [] := by
   have C := P.convex
-  induction V generalizing S with
+  set S := P.triangulation
+  match V, S with
+  | [], [] => simp [convex_triangulation] at C
+  | _ :: _, [] => simp [convex_triangulation] at C
+  | _ :: _, _ :: _ => simp
+
+lemma nodup (P : ConvexPolygon V) : Nodup V := by
+  have C := P.convex
+  set S := P.triangulation
+  induction V with
   | nil => simp
   | cons x V' IH =>
       match S, V' with
@@ -112,42 +116,41 @@ lemma nodup (P : ConvexPolygon V S) : Nodup V := by
       | [T], _ :: _ :: _ :: _ => simp [convex_triangulation] at C
       | T :: T' :: S', V' =>
         simp [convex_triangulation] at C; simp [nodup_cons]
-        exact ⟨ C.2.1.2.2.2.1 , IH (ConvexPolygon.mk C.1) C.1⟩
+        exact ⟨ C.2.1.2.2.2.1 , IH (ConvexPolygon.mk (T' :: S') C.1) C.1⟩
 
-lemma number_of_triangles_eq (P : ConvexPolygon V S) : S.length + 2 = P.n := by
+lemma number_of_triangles_eq (P : ConvexPolygon V) : P.triangulation.length + 2 = P.n := by
   have C := P.convex
+  have ne_nil := P.triangulation_ne_nil
+  generalize hS : P.triangulation = S at C ne_nil
   induction S generalizing V with
-  | nil => simp [convex_triangulation] at C
-  | cons T S' IHS =>
-      match V, S' with
-      | [], _ => simp [convex_triangulation] at C
-      | [_], [] => simp [convex_triangulation] at C
-      | [_, _], [] => simp [convex_triangulation] at C
-      | [_, _, _], [] => rfl
-      | _ :: _ :: _ :: _ :: V', [] => simp [convex_triangulation] at C
-      | x :: V', T' :: S'' =>
-          simp [ConvexPolygon.n]; simp [convex_triangulation] at C
-          have := IHS (ConvexPolygon.mk C.1) C.1
-          simp [ConvexPolygon.n] at this; rw [this]
+  | nil => contradiction
+  | cons T S' IH =>
+    match V, S' with
+    | [], _ => simp [convex_triangulation] at C
+    | [_], [] => simp [convex_triangulation] at C
+    | [_, _], [] => simp [convex_triangulation] at C
+    | [_, _, _], [] => rfl
+    | _ :: _ :: _ :: _ :: V', [] => simp [convex_triangulation] at C
+    | x :: V', T' :: S'' =>
+        simp [ConvexPolygon.n] at IH |-; simp [convex_triangulation] at C
+        exact IH (ConvexPolygon.mk (T' :: S'') C.1) (rfl) C.1
 
 end ConvexPolygon
 
--- version with implicit triangulation
-def is_convex (V : List point) : Prop := ∃ S, convex_triangulation V S
+lemma triangle_is_convex (T: Triangle) : ConvexPolygon [T.a, T.b, T.c] := ConvexPolygon.mk [T] (by tauto)
 
-lemma triangle_is_convex (T: Triangle) : ConvexPolygon [T.a, T.b, T.c] [T] := ConvexPolygon.mk (by tauto)
-
-lemma triangle_area_eq (P : ConvexPolygon V S) (abc : V = [a, b, c]) : P.area = area a b c := by
-  have := P.convex; rw [abc] at this
+lemma triangle_area_eq (P : ConvexPolygon [a,b,c]) : P.area = area a b c := by
+  have C := P.convex
+  set S := P.triangulation with ← hS
   match S with
-  | [] => exfalso; exact this
+  | [] => exfalso; exact C
   | [T] =>
       dsimp [ConvexPolygon.area, triangulation_area]; ring_nf
-      have : triangle_eq_of_pts a b c T := this
-      rcases this with (h|h|h|h|h|h); all_goals rw [h.1, h.2.1, h.2.2]; perm
+      have : triangle_eq_of_pts a b c T := C
+      rcases this with (h|h|h|h|h|h) <;> {rw [hS]; simp [triangulation_area, add_zero]; rw [h.1, h.2.1, h.2.2]; perm}
   | _ :: _ :: _ =>
-      have := P.number_of_triangles_eq
-      simp [ConvexPolygon.n, abc] at this
+    have := P.number_of_triangles_eq
+    simp [ConvexPolygon.n, hS] at this
 
 lemma nodup_of_paragram (pg: paragram a b c d M N O P) : Nodup [a, b, c, d] := by
   have := nodup_of_triangle $ tri124_of_paragram pg; simp [Nodup]; simp [Nodup] at this
@@ -157,7 +160,7 @@ lemma nodup_of_paragram (pg: paragram a b c d M N O P) : Nodup [a, b, c, d] := b
   have cd : c ≠ d := fun cd => by rw [← cd] at dP; exact not_para_of_inter cN dP pNP
   tauto
 
-lemma paragram_is_convex (pg: paragram a b c d M N O P) : is_convex [a, b, c, d] := by
+lemma paragram_is_convex (pg: paragram a b c d M N O P) : ConvexPolygon [a, b, c, d] := by
   have nodup := nodup_of_paragram pg; simp [Nodup, and_assoc] at nodup
   obtain ⟨ ab, ac, ad, bc, bd, cd⟩ := nodup
   let T := Triangle.mk b c d  bc bd cd
@@ -172,32 +175,34 @@ lemma paragram_is_convex (pg: paragram a b c d M N O P) : is_convex [a, b, c, d]
   constructor; left; exact sameside_of_para_online dO cO (by perma)
   constructor; exact sameside_of_para_online bN cN (by perma)
 
-lemma unique_triangulation (P : ConvexPolygon V S) (P' : ConvexPolygon V S') :
-    triangle_eq (S.head P.triangulation_ne_nil) (S'.head P'.triangulation_ne_nil) := by sorry
+lemma unique_triangulation (P : ConvexPolygon V) (P' : ConvexPolygon V):
+  triangle_eq (P.triangulation.head P.triangulation_ne_nil) (P'.triangulation.head P'.triangulation_ne_nil) := by
+  set S := P.triangulation with ← hS
+  set S' := P'.triangulation with ← hS'
+  simp [hS, hS']
+  sorry
+  -- untrue as stated, the last added triangle could be degenerate in different ways
 
 def diff_quadri_splits (T U T' U' : Triangle) : Prop :=
-  ∃ a b c d : point, ∃ V : List point,
-  ∃ _ : ConvexPolygon [a, b, c, d] [T, U],
-  ∃ _ : ConvexPolygon V [T', U'], [a, b, c, d] ≠ V ∧ Perm [a, b, c, d] V
+  ∃ a b c d : point, ∃ V : List point, ∃ P : ConvexPolygon [a, b, c, d], ∃ P' : ConvexPolygon V,
+  [a, b, c, d] ≠ V ∧ Perm [a, b, c, d] V ∧ P.triangulation = [T, U] ∧ P'.triangulation = [T', U']
 
-lemma eq_area_of_quadri (P : ConvexPolygon [a, b, c, d] [T, U]) (P' : ConvexPolygon V [T', U'])
-   (diff : [a, b, c, d] ≠ V) (perm : Perm [a, b, c, d] V) : P.area = P'.area := by
+lemma eq_area_of_quadri (P : ConvexPolygon [a, b, c, d]) (P' : ConvexPolygon V) (perm : Perm [a, b, c, d] V) : P.area = P'.area := by
   sorry
 
 def eq_area_of_quadri_splits (T U T' U' : Triangle) (splits : diff_quadri_splits T U T' U') :
     area T.a T.b T.c + area U.a U.b U.c = area T'.a T'.b T'.c + area U'.a U'.b U'.c := by
-  obtain ⟨ a, b, c, d, V, P, P', diff, perm ⟩ := splits
-  have := eq_area_of_quadri P P' diff perm
-  simp [ConvexPolygon.area, triangulation_area] at this
-  exact this
+  obtain ⟨ a, b, c, d, V, P, P', _, perm, hP, hP' ⟩ := splits
+  have := eq_area_of_quadri P P' perm
+  convert this <;> simp [ConvexPolygon.area, triangulation_area, hP, hP']
 
 def adj_triangulation (S : List Triangle) (S' : List Triangle) : Prop :=
   ∃ T U T' U' : Triangle, T ∈ S ∧ U ∈ S ∧ T' ∈ S' ∧ U' ∈ S' ∧
-  diff_quadri_splits T U T' U' ∧ Perm (S.diff [T, U]) (S'.diff [T', U'])
+  diff_quadri_splits T U T' U' ∧ Perm (S.diff [T, U]) (S'.diff [T', U']) ∧ (S.diff [T, U]) ≠ (S'.diff [T', U'])
 
-lemma eq_area_of_adj_triangulation (P : ConvexPolygon V S) (P' : ConvexPolygon V' S')
-    (adj : adj_triangulation S S') : P.area = P'area := by
+lemma eq_area_of_adj_triangulation (P : ConvexPolygon V) (P' : ConvexPolygon V')
+    (adj : adj_triangulation P.triangulation P'.triangulation) : P.area = P'area := by
   obtain ⟨ T, U, T', U', TS, US, T'S', U'S', splits, diff⟩ := adj
   sorry
 
-theorem area_invariance (P : ConvexPolygon V S) (P' : ConvexPolygon V' S') (perm: List.Perm V V') : P.area = P'.area := by sorry
+lemma eq_area_of_eq_last_vertex (P : ConvexPolygon (x :: V)) (P' : ConvexPolygon (x :: V')) (R : ConvexPolygon V) (R' : ConvexPolygon V') (H : R.area = R'.area ): P.area = P'.area := by sorry
